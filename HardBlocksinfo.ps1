ad295e8eb99fcbc543c1217af09f8756f1921aac
Madhu Sunke <MadhuSunke417@users.noreply.github.com>
2021-03-05 20:12:26 +0530

    Add files via upload

---
+++ HardBlocksinfo.ps1
@@ -0,0 +1,40 @@
+﻿#Madhu Sunke
+
+#To find Hardblocks information
+
+[hashtable]$Hash = @{}
+
+Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\TargetVersionUpgradeExperienceIndicators" -Recurse | ForEach-Object {
+       $path = Get-ItemProperty $_.PSPath
+
+       foreach($item in $path){
+
+       $GGatedBlockId = $item.GatedBlockId | Select-Object -ExpandProperty $_.GatedBlockId -ErrorAction SilentlyContinue
+       $RedReason     = $item.RedReason | Select-Object -ExpandProperty $_.RedReason -ErrorAction SilentlyContinue
+
+       if($GGatedBlockId -is [system.array]){
+       $Hash.Add("PreCache$($item.PSChildName)GatedBlockId",$($GGatedBlockId -join ";"))
+       }else{
+       $Hash.Add("PreCache$($item.PSChildName)GatedBlockId",$GGatedBlockId)
+       }
+
+       if($RedReason -is [system.array]){
+       $Hash.Add("PreCache$($item.PSChildName)RedReason",$($RedReason -join ";"))
+       }else{
+       $Hash.Add("PreCache$($item.PSChildName)RedReason",$RedReason)
+       }
+    
+       }
+
+ 
+ }
+
+$ourObject = New-Object -TypeName psobject -Property $Hash
+
+$task = Get-ScheduledTaskInfo -TaskName 'Microsoft Compatibility Appraiser' -TaskPath '\Microsoft\Windows\Application Experience\' -ErrorAction SilentlyContinue
+
+#Hardblock info
+$ourObject
+#Appraiser Task info
+$task
+
